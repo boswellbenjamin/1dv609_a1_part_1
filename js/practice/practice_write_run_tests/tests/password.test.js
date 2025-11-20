d34f6991411139a8141aa565f7d9ab1f2b101b8b
Benjamin Boswell <boswellbenn@hotmail.com>
2025-11-20 12:47:35 +0100

    Add constants for readability

--- js/practice/practice_write_run_tests/tests/password.test.js
+++ js/practice/practice_write_run_tests/tests/password.test.js
@@ -4,29 +4,27 @@
 // import { Password } from '../src/BugDoesNotHash'
 // import { Password } from '../src/BugDoesNotTrim'
 // import { Password } from '../src/BugisPasswordAlwaysSame'
- import { Password } from '../src/BugMissingNumberCheck'
+// import { Password } from '../src/BugMissingNumberCheck' // Done
 // import { Password } from '../src/BugMissingPasswordCheck'
 // import { Password } from '../src/BugNeverContainsNumbers'
 // import { Password } from '../src/BugToShortPassword'
 // import { Password } from '../src/BugVeryShort'
 // import { Password } from '../src/BugWrongHashingAlgorithm'
-// import { Password } from '../src/BugWrongMessage'
+// import { Password } from '../src/BugWrongMessage' // Done
 // import { Password } from '../src/Correct'
 
 describe('Password class, test suite', () => {
-    //put constants here to increase readability
+    const passwordNoNumber = 'passwordwithoutnumber'
+    const shortPassword = 'short'
     const emptyPassword = '';
-
-    test('replace this test with one of your own and add more', () => {
-        expect(true).toBe(true);
-    });
 
    test('Creating a Password with too short input should send Too short password error', () => {
        expect(() => {
-           new Password('short')}).toThrow('Too short password');
+           new Password(shortPassword)}).toThrow('Too short password');
    });
 
    test('Creating a Password without number should send No number found error', () => {
-    expect(() => {new Password('passwordwithoutnumber')}).toThrow('No number found')
+    expect(() => {
+       new Password(passwordNoNumber)}).toThrow('No number found')
     })
 });
