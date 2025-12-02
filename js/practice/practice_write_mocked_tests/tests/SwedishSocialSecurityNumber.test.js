650840a656d04fe51e49ce849d32fa8eab8251f2
Benjamin Boswell <boswellbenn@hotmail.com>
2025-12-02 14:13:36 +0100

    First mock test done

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,5 +1,6 @@
-// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber';  // Correct
-// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
+import { describe, test, expect, jest } from '@jest/globals'; 
+ import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber';  // Correct
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear' // Done
 // import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoLunh'
 // import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoTrim'
 // import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoLenCheck'
@@ -7,9 +8,19 @@
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
-    //put constants here to increase readability
 
-test('')
+test('Should return the correct year', () => {
+        const mockHelper = {
+        isCorrectLength: jest.fn().mockReturnValue(true),
+        isCorrectFormat: jest.fn().mockReturnValue(true),
+        isValidMonth: jest.fn().mockReturnValue(true),
+        isValidDay: jest.fn().mockReturnValue(true),
+        luhnisCorrect: jest.fn().mockReturnValue(true)
+    };
 
-    //Add your tests here
+    ssn = new SwedishSocialSecurityNumber('980818-3470', mockHelper)
+
+    expect(ssn.getYear()).toBe('98')
+})
+
 });
