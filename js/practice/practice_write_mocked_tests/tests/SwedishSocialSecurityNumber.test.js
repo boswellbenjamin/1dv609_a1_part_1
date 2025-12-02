9825210bc5b12d61f0439d1006cfbc67b969ed77
Benjamin Boswell <boswellbenn@hotmail.com>
2025-12-02 13:40:51 +0100

    Add imports for mocking

--- js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
+++ js/practice/practice_write_mocked_tests/tests/SwedishSocialSecurityNumber.test.js
@@ -1,14 +1,15 @@
-import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber'; 
-
+// import { SwedishSocialSecurityNumber } from '../src/correct/SwedishSocialSecurityNumber';  // Correct
+// import { SwedishSocialSecurityNumber } from '../src/bugs/BuggySwedishSocialSecutityNumberWrongYear'
+// import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoLunh'
+// import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoTrim'
+// import { SwedishSocialSecurityNumber } from '..src/bugs/BuggySwedishSocialSecutityNumberNoLenCheck'
 
 
 //NOTE THESE TESTS SHOULD NOT BE DEPENDENT ON SSNHelper BUT USE MOCKING
 describe('SwedishSocialSecurityNumber Tests', () => {
     //put constants here to increase readability
 
-    test('replace this test with one of your own', () => {
-        expect(true).toBe(true);
-    });
+test('')
 
     //Add your tests here
 });
